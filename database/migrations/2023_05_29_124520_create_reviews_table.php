4cffe88da69dc9ba5c0b847da721d3fe2e065ad6
maulanahafez <maulanahafez00@gmail.com>
2023-06-08 19:58:55 +0700

    fixed default review

--- database/migrations/2023_05_29_124520_create_reviews_table.php
+++ database/migrations/2023_05_29_124520_create_reviews_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id');
             $table->unsignedTinyInteger('rating');
             $table->text('review');
-            $table->string('visibility');
+            $table->string('visibility')->default('Show');
             $table->timestamps();
         });
     }
